0db9e8f01d3392768f0c1cb30469ffbab59725a4
renchenggong <renchenggong@shuidihuzhu.com>
2018-07-05 16:31:12 +0800

    存储数据的时候没必要用异步

--- db.js
+++ db.js
@@ -2,23 +2,6 @@ const Json2csvParser = require('json2csv').Parser
 const fs = require('fs')
 const dayjs = require('dayjs')
 const date = dayjs().format('YYYY_MM_DD')
-
-const saveFile = (filePath, fileData) => {
-  if(fs.existsSync(filePath)) {
-    fs.unlinkSync(filePath)
-  }
-  return new Promise((resolve, reject) => {
-    fs.writeFile(filePath, fileData, {
-      encoding: 'utf8'
-    }, err => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve()
-      }
-    })
-  })
-}
 class CSV {
   data(json, opts) {
     this.opts = opts || {}
@@ -30,20 +13,16 @@ class CSV {
     console.log(this.csv)
     return this
   }
-  async save(p) {
+  save(p) {
     let _p = p || `./data/${date}.csv`
-    if (!_p) {
-      throw new Error({
-        message: '没有指定路径'
-      })
-    }
-
+    let data = this.csv
     try {
-      await saveFile(_p, this.csv) // 这里的fileData是Buffer类型
+      fs.writeFileSync(_p, data, {
+        encoding: 'utf8'
+      })
     } catch (err) {
-      console.log(err)
+      console.warn(err)
     }
-    return this
   }
 }
 
